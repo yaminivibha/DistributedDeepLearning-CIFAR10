164277382c15fc364f72be1c322debebe1089f43
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-05-01 19:36:09 -0400

    fixing setup scripts

--- execute.sh
+++ execute.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Q1
-python3 lab5.py Q1 --batch_size 32 --outfile batchsize_32 .txt
-python3 lab5.py Q1 --batch_size 128 --outfile batchsize_128 .txt
-python3 lab5.py Q1 --batch_size 512 --outfile batchsize_512 .txt
+python3 lab5.py Q1 --batch_size 32 --outfile batchsize_32.txt
+python3 lab5.py Q1 --batch_size 128 --outfile batchsize_128.txt
+python3 lab5.py Q1 --batch_size 512 --outfile batchsize_512.txt
 
