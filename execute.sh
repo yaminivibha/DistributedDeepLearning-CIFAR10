10c8f7bfadefa440e1957fd685bb749c41fc2fc6
Yamini Ananth <75443071+yva2002@users.noreply.github.com>
2023-05-05 13:34:04 -0400

    2gpu

--- execute.sh
+++ execute.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Q1
-python3 lab5.py Q1 --batch_size 8 --gpus 4 --outfile 4GPU_batchsize_32.txt
-python3 lab5.py Q1 --batch_size 32 --gpus 4 --outfile 4GPU_batchsize_128.txt
-python3 lab5.py Q1 --batch_size 128 --gpus 4 --outfile 4GPU_batchsize_512.txt
-python3 lab5.py Q1 --batch_size 512 --gpus 4 --outfile 4GPU_batchsize_2048.txt
-python3 lab5.py Q1 --batch_size 2048 --gpus 4 --outfile 4GPU_batchsize_8192.txt
+# python3 lab5.py Q1 --batch_size 8 --gpus 4 --outfile 4GPU_batchsize_32.txt
+# python3 lab5.py Q1 --batch_size 32 --gpus 4 --outfile 4GPU_batchsize_128.txt
+# python3 lab5.py Q1 --batch_size 128 --gpus 4 --outfile 4GPU_batchsize_512.txt
+# python3 lab5.py Q1 --batch_size 512 --gpus 4 --outfile 4GPU_batchsize_2048.txt
+# python3 lab5.py Q1 --batch_size 2048 --gpus 4 --outfile 4GPU_batchsize_8192.txt
 
 
 python3 lab5.py Q1 --batch_size 16 --gpus 2 --outfile 2GPU_batchsize_32.txt
